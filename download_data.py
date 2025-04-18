18b3ef56eefd8453a3c5bda7b3db2e09a6eacc3b
NCSdecoopman <ncsdecoopman@gmail.com>
2025-04-18 10:08:11 +0200

    Ajout try except download file

--- download_data.py
+++ download_data.py
@@ -1,33 +1,44 @@
-# deploy/download_data.py
-
 from huggingface_hub import snapshot_download
+import traceback
 
-print("Téléchargement des fichiers GEV...")
-snapshot_download(
-    repo_id="ncsdecoopman/ExtremePrecipit",
-    repo_type="dataset",
-    revision="main",
-    local_dir="data",
-    cache_dir="/tmp/hf_cache",
-    allow_patterns=["gev/*"]
-)
-
-print("Téléchargement des métadonnées...")
-snapshot_download(
-    repo_id="ncsdecoopman/ExtremePrecipit",
-    repo_type="dataset",
-    revision="main",
-    local_dir="data",
-    cache_dir="/tmp/hf_cache",
-    allow_patterns=["metadonnees/*"]
-)
-
-print("Téléchargement des statistiques...")
-snapshot_download(
-    repo_id="ncsdecoopman/ExtremePrecipit",
-    repo_type="dataset",
-    revision="main",
-    local_dir="data",
-    cache_dir="/tmp/hf_cache",
-    allow_patterns=["statisticals/*"]
-)
+try:
+    print("Téléchargement des métadonnées...")
+    snapshot_download(
+        repo_id="ncsdecoopman/ExtremePrecipit",
+        repo_type="dataset",
+        revision="main",
+        local_dir="data",
+        cache_dir="/tmp/hf_cache",
+        allow_patterns=["metadonnees/*"]
+    )
+    print("Téléchargement des statistiques AROMES...")
+    snapshot_download(
+        repo_id="ncsdecoopman/ExtremePrecipit",
+        repo_type="dataset",
+        revision="main",
+        local_dir="data",
+        cache_dir="/tmp/hf_cache",
+        allow_patterns=["statisticals/modelised*"]
+    )
+    print("Téléchargement des statistiques STATIONS...")
+    snapshot_download(
+        repo_id="ncsdecoopman/ExtremePrecipit",
+        repo_type="dataset",
+        revision="main",
+        local_dir="data",
+        cache_dir="/tmp/hf_cache",
+        allow_patterns=["statisticals/observed*"]
+    )
+    # print("Téléchargement des fichiers GEV...")
+    # snapshot_download(
+    #     repo_id="ncsdecoopman/ExtremePrecipit",
+    #     repo_type="dataset",
+    #     revision="main",
+    #     local_dir="data",
+    #     cache_dir="/tmp/hf_cache",
+    #     allow_patterns=["gev/*"]
+    # )
+except Exception as e:
+    print("Erreur pendant le téléchargement :")
+    traceback.print_exc()
+    raise SystemExit(1)
