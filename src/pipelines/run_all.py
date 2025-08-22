5a169e7f3c3a02474f24c49faef76c4a49844a1a
NCSdecoopman <ncsdecoopman@gmail.com>
2025-08-22 18:44:22 +0200

    MAJ RAPPORT v1

--- src/pipelines/run_all.py
+++ src/pipelines/run_all.py
@@ -205,14 +205,14 @@ SEASONS = SEASON_SEAS + SEASON_MONTHS
 log(f"Lancement des générations de maps")
 
 
-for data_type in ["gev"]: # "stats", "gev", "dispo"
+for data_type in ["gev"]: #"dispo", "stats", "gev"
                 
     if data_type == "dispo":
         COL_CALCULATE = ["n_years"]
     elif data_type == "stats":
-        COL_CALCULATE = ["numday", "mean", "mean-max"] # 
+        COL_CALCULATE = ["mean-max"] # "numday", "mean", 
     elif data_type == "gev":
-        COL_CALCULATE = ["z_T_p"] # "significant", "model"
+        COL_CALCULATE = ["model"] # "significant", "z_T_p", 
 
     for col_calculate in COL_CALCULATE:
 
@@ -232,7 +232,7 @@ for data_type in ["gev"]: # "stats", "gev", "dispo"
                     sat = 90
 
             if echelle == "quotidien":            
-                DIFFERENTE_PERIODE = [False] # True
+                DIFFERENTE_PERIODE = [False, True] # 
             else:            
                 DIFFERENTE_PERIODE = [False]
 
@@ -241,10 +241,10 @@ for data_type in ["gev"]: # "stats", "gev", "dispo"
                 if data_type == "dispo":
                     SEASON_GENERATE = [["hydro"]]
                 elif data_type == "stats":
-                    if col_calculate in ["numday"]:
-                        SEASON_GENERATE = [["hydro"], SEASON_SEAS] # hydro doit être calculer séparement pour l'échelle "jours"
-                    else:
+                    if col_calculate == "mean-max":
                         SEASON_GENERATE = [["hydro", *SEASON_SEAS]]
+                    else:
+                        SEASON_GENERATE = [["hydro"], SEASON_SEAS] # hydro doit être calculer séparement
                 else:
                     SEASON_GENERATE = [["hydro", *SEASON_SEAS], SEASON_MONTHS]
 
